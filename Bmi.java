d697e1d46fac13fbd9ea8680c960c809fba5db11
fujiwarakanato <K.fujiwara.sys23@morijyobi.ac.jp>
2023-11-10 11:01:15 +0900

    名前の変更

---
+++ Bmi.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Bmi {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double height1 = scanner.nextDouble();
+        double weight1 = scanner.nextDouble();
+        double BMI1 = weight1 / (height1 * height1);
+
+        String surume1 = String.format("%.2f", BMI1);
+        System.out.println(surume1);
+        double height2 = scanner.nextDouble();
+        double weight2 = scanner.nextDouble();
+        double BMI2 = weight2 / (height2 * height2);
+        String surume2 = String.format("%.2f", BMI2);
+        System.out.println(surume2);
+    }
+}
+}
