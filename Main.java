5b699d4b79c879674135e62152405a2ce78e1d80
fujiwarakanato <K.fujiwara.sys23@morijyobi.ac.jp>
2023-11-10 10:37:44 +0900

    一回の実行で二人分計算できるように仕様変更

--- Main.java
+++ Main.java
@@ -3,10 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double height = scanner.nextDouble();
-        double weight = scanner.nextDouble();
-        double BMI = weight / (height * height);
-        String surume = String.format("%.2f", BMI);
-        System.out.println(surume);
+        double height1 = scanner.nextDouble();
+        double weight1 = scanner.nextDouble();
+        double BMI1 = weight1 / (height1 * height1);
+
+        String surume1 = String.format("%.2f", BMI1);
+        System.out.println(surume1);
+        double height2 = scanner.nextDouble();
+        double weight2 = scanner.nextDouble();
+        double BMI2 = weight2 / (height2 * height2);
+        String surume2 = String.format("%.2f", BMI2);
+        System.out.println(surume2);
     }
+}
 }
